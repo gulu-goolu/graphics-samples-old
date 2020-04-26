15a7aa21e442043e60d36734a457fb307102d507
murmur wheel <murmur.wheel@gmail.com>
2020-04-27 01:23:43 +0800

    buffer creation and destroy

--- include/vkut/buffer.h
+++ include/vkut/buffer.h
@@ -5,4 +5,35 @@
 #ifndef VKUT_BUFFER_H
 #define VKUT_BUFFER_H
 
+#include "device.h"
+#include <memory>
+
+namespace vkut {
+class Buffer;
+using BufferPtr = std::shared_ptr<Buffer>;
+
+class Buffer {
+public:
+    explicit Buffer(VkDeviceSize size,
+        VkBufferUsageFlags usage,
+        VkMemoryPropertyFlags memory_flags);
+    ~Buffer();
+
+    [[nodiscard]] const VkBuffer &vk_buffer() const { return vk_buffer_; }
+    [[nodiscard]] const VkDeviceMemory &vk_device_memory() const {
+        return vk_device_memory_;
+    }
+
+private:
+    VkBuffer vk_buffer_{ VK_NULL_HANDLE };
+    VkDeviceMemory vk_device_memory_{ VK_NULL_HANDLE };
+
+    // creation arguments
+    VkDeviceSize size_{ 0 };
+    VkBufferUsageFlags usage_{ 0 };
+    VkMemoryPropertyFlags memory_flags_{ 0 };
+};
+
+}
+
 #endif // VKUT_BUFFER_H
--- src/buffer.cpp
+++ src/buffer.cpp
@@ -3,3 +3,50 @@
 //
 
 #include <vkut/buffer.h>
+
+namespace vkut {
+Buffer::Buffer(VkDeviceSize size,
+    VkBufferUsageFlags usage,
+    VkMemoryPropertyFlags memory_flags) :
+  size_(size),
+  usage_(usage), memory_flags_(memory_flags) {
+    auto device = Device::get();
+
+    // create buffer
+    VkBufferCreateInfo create_info = {};
+    create_info.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+    create_info.size = size;
+    create_info.usage = usage;
+    create_info.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+    create_info.queueFamilyIndexCount = 0;
+    create_info.pQueueFamilyIndices = nullptr;
+    VKUT_THROW_IF_FAILED(vkCreateBuffer(
+        device->vk_device(), &create_info, nullptr, &vk_buffer_));
+
+    // allocate memory
+    VkMemoryRequirements req;
+    vkGetBufferMemoryRequirements(device->vk_device(), vk_buffer_, &req);
+
+    VkMemoryAllocateInfo allocate_info = {};
+    allocate_info.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+    allocate_info.memoryTypeIndex =
+        device->get_memory_type(req.memoryTypeBits, memory_flags);
+    allocate_info.allocationSize = req.size;
+    VKUT_THROW_IF_FAILED(vkAllocateMemory(
+        device->vk_device(), &allocate_info, nullptr, &vk_device_memory_));
+
+    // bind memory
+    VKUT_THROW_IF_FAILED(vkBindBufferMemory(
+        device->vk_device(), vk_buffer_, vk_device_memory_, 0));
+}
+
+Buffer::~Buffer() {
+    auto device = Device::get();
+    if (vk_buffer_) {
+        vkDestroyBuffer(device->vk_device(), vk_buffer_, nullptr);
+    }
+    if (vk_device_memory_) {
+        vkFreeMemory(device->vk_device(), vk_device_memory_, nullptr);
+    }
+}
+}
